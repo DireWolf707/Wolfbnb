aa337902c3fae72e67f400f6322a44a2ffefd2b7
Direwolf <rahulsood707@gmail.com>
2025-01-19 14:48:56 +0530

    added create listing form

--- app/create-listing/page.tsx
+++ app/create-listing/page.tsx
@@ -3,119 +3,207 @@ import React, { useState } from 'react'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from '@/components/ui/form'
-import { listingT, STEPS, unsafeListingT } from '@/lib/types'
+    categortyT,
+    unsafeCreateListingT,
+    createListingT,
+    createListingFormStepT,
+    MyFile,
+    STEPS,
+} from '@/lib/types'
 import { listingSchema } from '@/lib/zodSchemas'
-import CategoryInput from '@/components/listing/CategoryInput'
-import FormNavigationButton from '@/components/listing/FormNavigationButton'
-import FormHeading from '@/components/listing/FormHeading'
-import FormWrapper from '@/components/listing/FormWrapper'
-import LocationInput from '@/components/listing/LocationInput'
-import CounterInput from '@/components/listing/CounterInput'
+import CategoryInput from '@/components/listing/create/CategoryInput'
+import FormNavigationButton from '@/components/listing/create/FormNavigationButton'
+import LocationInput from '@/components/listing/create/LocationInput'
+import CounterInput from '@/components/listing/create/CounterInput'
+import ImageUpload from '@/components/listing/create/ImageUpload'
+import Image from 'next/image'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
+import FormError from '@/components/listing/create/FormError'
+import { DollarSignIcon } from 'lucide-react'
+import Heading from '@/components/layout/Heading'
 
-const defaultValues: unsafeListingT = {
+const defaultValues: unsafeCreateListingT = {
     guestCount: 1,
     roomCount: 1,
     bathroomCount: 1,
     price: 1,
 }
 
+const formFields: createListingFormStepT = {
+    [STEPS.CATEGORY]: ['category'],
+    [STEPS.LOCATION]: ['location'],
+    [STEPS.INFO]: ['bathroomCount', 'guestCount', 'roomCount'],
+    [STEPS.IMAGE]: ['image'],
+    [STEPS.DESCRIPTION]: ['title', 'description'],
+    [STEPS.PRICE]: ['price'],
+}
+
 const CreateListing = () => {
     const [step, setStep] = useState(STEPS.CATEGORY)
-    const form = useForm<listingT>({
+    const {
+        watch,
+        register,
+        handleSubmit,
+        setValue,
+        trigger,
+        formState: { errors },
+    } = useForm<createListingT>({
         resolver: zodResolver(listingSchema),
         defaultValues,
     })
 
-    if (step == STEPS.CATEGORY)
-        return (
-            <FormWrapper>
-                <FormHeading
-                    title="Which of these best describes your place?"
-                    subtitle="Pick a category"
-                />
-                <CategoryInput
-                    value={form.watch('category')}
-                    onClick={(val: string) => form.setValue('category', val)}
-                />
-                <FormNavigationButton step={step} setStep={setStep} />
-            </FormWrapper>
-        )
+    const image = watch('image') as MyFile
 
-    if (step == STEPS.LOCATION)
-        return (
-            <FormWrapper>
-                <FormHeading
-                    title="Where is your place located?"
-                    subtitle="Help guests find you!"
-                />
-                <LocationInput
-                    value={form.watch('location')}
-                    onClick={(val: string) => form.setValue('location', val)}
-                />
-                <FormNavigationButton step={step} setStep={setStep} />
-            </FormWrapper>
-        )
+    return (
+        <form className="mx-auto my-6 flex w-[480px] flex-col gap-8 overflow-y-auto rounded-xl border-2 p-4">
+            {step == STEPS.CATEGORY && (
+                <>
+                    <Heading
+                        title="Which of these best describes your place?"
+                        subtitle="Pick a category"
+                        err={errors.category}
+                    />
 
-    if (step == STEPS.INFO)
-        return (
-            <FormWrapper>
-                <FormHeading
-                    title="Share some basics about your place"
-                    subtitle="What amenities do you have?"
-                />
-                <div className="flex flex-col gap-4 overflow-auto">
-                    <CounterInput
-                        title="Guests"
-                        subtitle="How many guests do you allow?"
-                        value={form.watch('guestCount')}
-                        onChange={(val: number) =>
-                            form.setValue('guestCount', val)
-                        }
+                    <CategoryInput
+                        value={watch('category')}
+                        onClick={(val: categortyT) => setValue('category', val)}
                     />
-                    <hr />
-                    <CounterInput
-                        title="Rooms"
-                        subtitle="How many rooms do you have?"
-                        value={form.watch('roomCount')}
-                        onChange={(val: number) =>
-                            form.setValue('roomCount', val)
-                        }
-                    />
-                    <hr />
-                    <CounterInput
-                        title="Bathrooms"
-                        subtitle="How many bathrooms do you have?"
-                        value={form.watch('bathroomCount')}
-                        onChange={(val: number) =>
-                            form.setValue('bathroomCount', val)
-                        }
-                    />
-                </div>
-                <FormNavigationButton step={step} setStep={setStep} />
-            </FormWrapper>
-        )
+                </>
+            )}
 
-    if (step == STEPS.IMAGES)
-        return (
-            <FormWrapper>
-                <FormHeading
-                    title="Add a photos of your place"
-                    subtitle="Show guests what you place looks like!"
-                />
-                {/* <LocationInput
-                    value={form.watch('location')}
-                    onClick={(val: string) => form.setValue('location', val)}
-                /> */}
-                <FormNavigationButton step={step} setStep={setStep} />
-            </FormWrapper>
-        )
+            {step == STEPS.LOCATION && (
+                <>
+                    <Heading
+                        title="Where is your place located?"
+                        subtitle="Help guests find you!"
+                        err={errors.location}
+                    />
+
+                    <LocationInput
+                        value={watch('location')}
+                        onClick={(val: string) => setValue('location', val)}
+                    />
+                </>
+            )}
+
+            {step == STEPS.INFO && (
+                <>
+                    <Heading
+                        title="Share some basics about your place"
+                        subtitle="What amenities do you have?"
+                    />
+
+                    <div className="flex flex-col gap-4 overflow-auto">
+                        <CounterInput
+                            title="Guests"
+                            subtitle="How many guests do you allow?"
+                            value={watch('guestCount')}
+                            onChange={(val: number) =>
+                                setValue('guestCount', val)
+                            }
+                        />
+                        <hr />
+                        <CounterInput
+                            title="Rooms"
+                            subtitle="How many rooms do you have?"
+                            value={watch('roomCount')}
+                            onChange={(val: number) =>
+                                setValue('roomCount', val)
+                            }
+                        />
+                        <hr />
+                        <CounterInput
+                            title="Bathrooms"
+                            subtitle="How many bathrooms do you have?"
+                            value={watch('bathroomCount')}
+                            onChange={(val: number) =>
+                                setValue('bathroomCount', val)
+                            }
+                        />
+                    </div>
+                </>
+            )}
+
+            {step == STEPS.IMAGE && (
+                <>
+                    <Heading
+                        title="Add a photos of your place"
+                        subtitle="Show guests what you place looks like!"
+                        err={errors.image}
+                    />
+
+                    <div className="flex flex-col gap-4">
+                        {image?.preview && (
+                            <Image
+                                className="mx-auto aspect-square rounded-xl object-cover"
+                                src={image.preview!}
+                                alt="image"
+                                height={200}
+                                width={200}
+                            />
+                        )}
+
+                        <ImageUpload
+                            file={image}
+                            setFile={(file: MyFile) => setValue('image', file)}
+                            disabled={false}
+                        />
+                    </div>
+                </>
+            )}
+
+            {step == STEPS.DESCRIPTION && (
+                <>
+                    <Heading
+                        title="How would you describe your place?"
+                        subtitle="Short and sweet works best!"
+                    />
+
+                    <div className="flex flex-col gap-4">
+                        <Label>Title</Label>
+                        <Input {...register('title')} />
+                        <FormError err={errors.title} />
+
+                        <Label>Description</Label>
+                        <Textarea {...register('description')} />
+                        <FormError err={errors.description} />
+                    </div>
+                </>
+            )}
+
+            {step == STEPS.PRICE && (
+                <>
+                    <Heading
+                        title="Set your price"
+                        subtitle="How much do you charge per night?"
+                    />
+
+                    <div className="flex flex-col gap-4">
+                        <Label>Price</Label>
+                        <div className="flex items-center gap-2">
+                            <DollarSignIcon className="size-5" />
+                            <Input
+                                type="number"
+                                {...register('price', { valueAsNumber: true })}
+                                min={1}
+                            />
+                        </div>
+                        <FormError err={errors.price} />
+                    </div>
+                </>
+            )}
+
+            <FormNavigationButton
+                handleSubmit={handleSubmit}
+                formFields={formFields}
+                trigger={trigger}
+                step={step}
+                setStep={setStep}
+            />
+        </form>
+    )
 }
 
 export default CreateListing
