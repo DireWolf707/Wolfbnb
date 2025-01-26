8483415458518f8c7a2163eafc895f25cdba3978
Direwolf <rahulsood707@gmail.com>
2025-01-27 00:21:31 +0530

    disabled github auth

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth'
 import Google from 'next-auth/providers/google'
-import GitHub from 'next-auth/providers/github'
+// import GitHub from 'next-auth/providers/github'
 import { DrizzleAdapter } from '@auth/drizzle-adapter'
 import db from './drizzle/client'
 import { userTable, accountTable } from './drizzle/schema'
@@ -10,7 +10,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         usersTable: userTable,
         accountsTable: accountTable,
     }),
-    providers: [Google, GitHub],
+    providers: [
+        Google,
+        //  GitHub
+    ],
     session: {
         strategy: 'jwt',
     },
